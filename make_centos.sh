4e523f96fa00bd96fde3e7636c0be5a2414a0ee2
shrikanthkr <shrikanth_kr@yahoo.com>
2020-11-28 16:57:35 +0530

    more fixes

--- make_centos.sh
+++ make_centos.sh
@@ -91,16 +91,16 @@ function fetch_custom_rpms(){
     yum install -y yum-utils
 
     echo "Downloading Extra repos"
-    yumdownloader --enablerepo=extras epel-release 
+    yumdownloader --enablerepo=extras epel-release --destdir=./rpms
 
     echo "Downloading iptables"
-    yumdownloader iptables-services
+    yumdownloader iptables-services --destdir=./rpms
 
     echo "Adding config manager"
     yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
     echo "Downloading Docker"
-    yumdownloader  docker-ce docker-ce-cli containerd.io
+    yumdownloader  docker-ce docker-ce-cli containerd.io --destdir=./rpms
 }
 function copy_rpms() {
     echo "Copying custom RPMS"
