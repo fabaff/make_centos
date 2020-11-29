682cf30b54e7c0bbd982dc67d02bfc73b813c46c
shrikanthkr <shrikanth_kr@yahoo.com>
2020-11-29 14:04:18 +0530

    try focring compression

--- make_centos.sh
+++ make_centos.sh
@@ -154,7 +154,7 @@ function create_iso() {
     echo "Creating Repo"
     cat $DVD_LAYOUT/$REPODATA/comps.xml
 
-    /usr/bin/createrepo -v -g --compress-type=gz repodata/comps.xml $DVD_LAYOUT/BaseOS
+    /usr/bin/createrepo -v -g repodata/comps.xml $DVD_LAYOUT/BaseOS --compress-type=bz2
     echo "Creating Repo Completed"
     echo "Creating new ISO image ..."
     if [ ! -e /usr/bin/genisoimage ]; then
